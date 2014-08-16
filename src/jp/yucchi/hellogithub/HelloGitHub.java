f9718880373efea37d1a8b38172d03a9e59d1c16
yucchi <yucchi@yucchi-impul>
2014-08-16 19:49:51 +0900

    暗号文を追加

--- src/jp/yucchi/hellogithub/HelloGitHub.java
+++ src/jp/yucchi/hellogithub/HelloGitHub.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package jp.yucchi.hellogithub;
 
 /**
@@ -16,7 +10,9 @@ public class HelloGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("この暗号文を解読せよ。");
+        System.out.println("15,16,16,1,9,23,15,23,1,18,1,21,13,15,14,15,8,1,15,16,16,1,9,14,1,11,21");
     }
-    
+
 }
